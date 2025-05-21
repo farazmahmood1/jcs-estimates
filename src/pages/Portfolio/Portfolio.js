53dc9b57e237e7aaa661f45248e101da4436a727
muhammadjunaidakhter100 <muhammadjunaidakhter100@gmail.com>
2025-05-21 23:43:54 +0500

    updated the code

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,9 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import projects from './projects';
 import './Portfolio.css';
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const Portfolio = () => {
-  const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedProject, setSelectedProject] = useState(null); 
+   const carouselRef = useRef(null);
+  
+    const options = {
+      responsiveClass: true,
+      dots: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        400: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        700: {
+          items: 1,
+        },
+        1000: {
+          items: 1,
+        },
+      },
+    };
+  
+    const handleNext = () => {
+      if (carouselRef.current) {
+        carouselRef.current.next();
+      }
+    };
+  
+    const handlePrev = () => {
+      if (carouselRef.current) {
+        carouselRef.current.prev();
+      }
+    };
 
   const handleReadMore = (project) => {
     setSelectedProject(project);
@@ -53,9 +93,10 @@ const Portfolio = () => {
         </div>
 
         <div className="projects-list">
+
           {projects.map((project, index) => (
-            <div className="project-item" key={index} onClick={() => handleReadMore(project)}>
-              <div className="project-thumbnail">
+            <div className="project-item" style={{ width: index === 0 ? "100%" : "" }} key={index} onClick={() => handleReadMore(project)}>
+              <div className="project-thumbnail" style={{ height: index === 0 ? "300px" : "" }}>
                 <img src={project.images[0]} alt={project.name} className="thumbnail-img" />
                 <div className="project-title">
                   <h3>{project.title}</h3>
@@ -78,14 +119,21 @@ const Portfolio = () => {
             <div className="project-popup-content">
               <h2>{selectedProject.title}</h2>
               <p>{selectedProject.description}</p>
-              <div className="excel-container">
-                <img src={selectedProject.images[0]} alt={selectedProject.name} className="thumbnail-img" />
 
-              </div>
+              <OwlCarousel className="owl-theme"
+          ref={carouselRef}
+          {...options}>
+                {selectedProject.images.map((img, idx) => (
+                  <div key={idx} className="excel-container">
+                    <img src={img} alt={`${selectedProject.title}-${idx}`} className="thumbnail-img" />
+                  </div>
+                ))}
+              </OwlCarousel>
             </div>
           </div>
         </div>
       )}
+
     </div>
   );
 };
